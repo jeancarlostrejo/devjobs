b735c6b4bfae2364198be91f05de95ab52a02a35
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-10 16:34:30 -0400

    add routing and model controller and migration to create form to vacant

---
+++ app/Http/Controllers/VacantController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vacant;
+use Illuminate\Http\Request;
+
+class VacantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('vacants.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('vacants.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vacant $vacant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vacant $vacant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Vacant $vacant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vacant $vacant)
+    {
+        //
+    }
+}
