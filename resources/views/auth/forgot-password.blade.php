021bdaad9d08866d2cad3298794ff1e2936bb8ad
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-10 01:05:13 -0400

    modify design for forgot password view

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -16,10 +16,17 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div class="flex justify-between my-5">
+            <x-link :href="route('login')">
+                    {{ __('Log in') }}
+            </x-link>
+            <x-link :href="route('register')">
+                    {{ __('Create account') }}
+            </x-link>
         </div>
+
+        <x-primary-button class="w-full justify-center">
+            {{ __('Email Password Reset Link') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
