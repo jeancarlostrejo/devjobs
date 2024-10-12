4c1e4720caebd66aad169912f44326c73b19e544
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-12 14:30:16 -0400

    change style to message flash for created vacant

--- resources/views/vacants/index.blade.php
+++ resources/views/vacants/index.blade.php
@@ -7,11 +7,18 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
             @if (session()->has('message'))
-                <div class="uppercase border border-green-600 bg-green-100 text-green-600 font-bold p-2 my-3 text-sm">
-                    {{ session('message') }}
-                </div>
+            <div class="flex justify-between text-sm bg-green-300 px-4 py-3 my-2 rounded text-center mx-2 md:mx-0"
+                role="alert">
+                <span class="text-green-900 font-bold">{{ session('message') }}</span>
+                <span class="inline" onclick="return this.parentNode.remove();">
+                    <svg class="fill-green-900 h-6 w-6" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <title>{{ __('Close') }}</title>
+                        <path
+                            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                    </svg>
+                </span>
+            </div>
             @endif
 
             <livewire:show-vacants>
