921ae644d09241006fa5b6781ea9f3444b2f57db
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-15 14:38:07 -0400

    add validation rules to cv field

--- app/Livewire/ApplyVacancy.php
+++ app/Livewire/ApplyVacancy.php
@@ -2,10 +2,23 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ApplyVacancy extends Component
 {
+    use WithFileUploads;
+
+    #[Validate('required|mimes:pdf')]
+    public $cv;
+
+    public function apply()
+    {
+        $this->validate();
+
+
+    }
     public function render()
     {
         return view('livewire.apply-vacancy');
--- resources/views/livewire/apply-vacancy.blade.php
+++ resources/views/livewire/apply-vacancy.blade.php
@@ -1,10 +1,11 @@
 <div class="bg-gray-100 p-5 mt-10 flex flex-col overflow-x-hidden md:justify-center md:items-center">
     <h3 class="text-center text-2xl font-bold my-4">{{ __('Apply for this vacancy') }}</h3>
 
-    <form class="w-96 mt-5">
+    <form wire:submit='apply' class="w-96 mt-5">
         <div class="mb-4">
             <x-input-label for="cv" :value="__('Curriculum vitae or resume (PDF)')" />
-            <x-text-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full" />
+            <x-text-input id="cv" type="file" wire:model='cv' accept=".pdf" class="block mt-1 w-full" />
+            <x-input-error :messages="$errors->get('cv')" class="mt-2" />
         </div>
 
         <x-primary-button class="my-5">{{ __('Apply') }}</x-primary-button>
