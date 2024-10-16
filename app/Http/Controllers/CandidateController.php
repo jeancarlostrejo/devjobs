4af23ed8e48fe1e88880bf80b3726ab8fde146f7
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-16 13:28:25 -0400

    add view to list candidates for a vacancy

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Vacant;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class CandidateController extends Controller
 {
-    //
+    public function index(Vacant $vacant): View
+    {
+        return view('candidates.index', compact('vacant '));
+    }
 }
