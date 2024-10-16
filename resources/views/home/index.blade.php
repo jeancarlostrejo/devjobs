f31d73413d71df9cc52ec9b598ef71758c68f601
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-16 16:31:51 -0400

    add initial view homepage

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <div class="py-16 bg-gray-50 lg:py-24">
+        <div class="max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+            <div class="">
+                <h2 class="text-center text-4xl leading-8 font-bold tracking-tight text-indigo-600 sm:text-6xl">Encuentra un trabajo en Tech de forma remota</h2>
+                <p class="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">{{ __("Encuentra el trabajo de tus sueños en una empresa internacional; tenemos vacantes para frontend developer, backend, devops y mucho más!") }}</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
