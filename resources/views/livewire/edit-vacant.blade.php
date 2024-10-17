b429f390a4d3a9af6996727c7b7b3d6cb23a8f60
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-16 21:59:52 -0400

    fix error in edit view with image overflow the screen in small device

--- resources/views/livewire/edit-vacant.blade.php
+++ resources/views/livewire/edit-vacant.blade.php
@@ -53,12 +53,12 @@
             <p class="text-sm">{{ __("Loading preview image") }}...</p>
         </div>
 
-        <div class="my-5 w-80">
+        <div class="my-5 md:w-80">
             <x-input-label :value="__('Current image')" />
             <img src="{{ Storage::url($image) }}" alt="{{ __('Vacant image') . " ".  $title }}">
         </div>
 
-        <div class="my-5 w-80">
+        <div class="my-5 md:w-80">
             @if($newImage)
                 <x-input-label :value="__('New image')" />
                 <img src="{{ $newImage->temporaryUrl() }}" alt="{{ __('Vacant image') }}">
